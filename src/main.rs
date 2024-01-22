15bd5b0e87faeb45923b0e7517485802f4b7a251
jonathan hamilton <longjonns@gmail.com>
2024-01-22 20:20:19 +0000

    got the form working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, post, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use handlebars::Handlebars;
 use std::collections::HashMap;
+
+#[derive(Deserialize)]
+struct FormData {
+    name: String,
+}
 
 async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
     let data = HashMap::<&str, &str>::new();
     let body = hb.render("index", &data).unwrap();
 
     HttpResponse::Ok().body(body)
+}
+
+#[post("/form")]
+async fn form(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().body(format!("name: {}", form.name))
 }
 
 #[actix_web::main]
@@ -20,6 +31,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(handlebars.clone()))
             .route("/", web::get().to(index))
+            .service(form)
     })
     .bind("127.0.0.1:8080")?
     .run()
